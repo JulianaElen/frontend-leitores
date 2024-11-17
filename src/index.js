fbd4d3463110ff1a658faf91e652869b95d716b1
JulianaElen <julianaesoliveira@gmail.com>
2024-11-17 16:05:25 -0300

    footer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import MenuPrincipal from './MenuPrincipal'
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <MenuPrincipal />
     <App />
+    <Footer />
   </>
 );
