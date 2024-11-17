15f3a4b6a62fe7f5139bd87f6278095c34a90843
JulianaElen <julianaesoliveira@gmail.com>
2024-11-17 16:49:38 -0300

    Pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import './App.css'
+import logo from './image/logo-png.png';
 
 export default function App() {
   return (
-    <div></div>
+    <div className="main-content">
+    <div className="image-container">
+    <img src={logo} className="main-image" alt="Logo" />
+      <h1 className="main-title">Leitores</h1>
+    </div>
+    <button className="iniciar-pesquisa-btn">Iniciar Pesquisa</button>
+  </div>
   )
 }
