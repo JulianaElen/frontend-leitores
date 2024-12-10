9d25820fd4434678345f0df93f9aaabba9acc9f5
JulianaElen <julianaesoliveira@gmail.com>
2024-12-10 17:53:05 -0300

    Link Vercel Funcionando

--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -8,21 +8,13 @@ export default function Sobre() {
                 <div id='texto-sobre'>
                 <h1>Sobre o Projeto</h1>
                     <p>
-                        O projeto "Perfil dos Leitores" foi criado com o intuito de entender melhor os hábitos, preferências
-                        e comportamentos dos leitores. Através de uma pesquisa detalhada, buscamos explorar o que motiva a escolha
-                        de livros, como a leitura se encaixa na rotina dos participantes e quais são os principais fatores influentes
-                        na decisão de leitura.
+                        O projeto "Perfil dos Leitores" foi criado com o intuito de entender melhor os hábitos, preferências e comportamentos dos leitores. Através de uma pesquisa detalhada, busquei explorar o que motiva a escolha de livros, como a leitura se encaixa na rotina dos participantes e quais são os principais fatores influentes na decisão de leitura.
                     </p>
                     <p>
-                        Este trabalho é uma iniciativa acadêmica que visa contribuir para a compreensão das tendências do mercado
-                        editorial e dos hábitos culturais de leitura. Os dados coletados são analisados para proporcionar uma visão
-                        clara sobre os perfis dos leitores, ajudando editoras, livrarias e até mesmo autores a desenvolverem melhores
-                        estratégias para se conectar com seus públicos.
+                        Este projeto foi apresentado à disciplina de Desenvolvimento de Sistemas WEB, ministrada pelo Professor Dr. Eduardo Ribeiro Felipe, do Curso de Engenharia de Computação da Universidade Federal de Itajubá - Campus Itabira, como requisito para avalizção da N2.
                     </p>
                     <p>
-                        Nossa missão é promover uma reflexão sobre o impacto da leitura no desenvolvimento pessoal e social, além de
-                        criar um ambiente interativo para que todos possam compartilhar suas opiniões sobre livros, gêneros e
-                        formatos preferidos.
+                        Aluna responsável: Juliana Elen Silva Oliveira. RA: 2021005540. E-mail: d2021005540@unifei.edu.br
                     </p>
                 </div>
             </div>
